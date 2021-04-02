3a814e2ea9ed12319098c676d518391d624f895b
Oudom-boyo <58159737+Oudom-boyo@users.noreply.github.com>
2021-04-02 22:33:13 +0700

    [Task] 3 Assignment
    This is Task 3 WCT

---
+++ Lab_3/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+$country =  null;
+
+if ($session !== null) {
+  $user = $session->user;
+
+  if ($user !== null) {
+    $address = $user->getAddress();
+ 
+    if ($address !== null) {
+      $country = $address->country;
+    }
+  }
+
+}
+
+
+
+?>
